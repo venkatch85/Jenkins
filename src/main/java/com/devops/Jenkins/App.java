be7a7571016835bfe4ae077251a6ea9f47ef4c19
Venkat <venky.cheruku@gmail.com>
2020-07-23 14:45:55 -0400

    First commit jenkins project

---
+++ src/main/java/com/devops/Jenkins/App.java
@@ -0,0 +1,10 @@
+package com.devops.Jenkins;
+
+
+public class App 
+{
+	public static void hello()
+	{
+		System.out.println("This is a jenkins project");
+    }
+}
---
+++ src/test/java/com/devops/Jenkins/TestingJenkins.java
@@ -0,0 +1,15 @@
+package com.devops.Jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestingJenkins {
+
+	@Test
+	public void test()
+	{
+		App.hello();
+	}
+
+}
